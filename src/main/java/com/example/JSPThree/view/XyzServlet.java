a6a43f70cc9971d97687676fb732ea2a8e7d4bcc
ebonynon <sithumsandeepap@gmail.com>
2021-04-16 22:30:25 +0530

    Rm unused blocks

--- src/main/java/com/example/JSPThree/view/XyzServlet.java
+++ src/main/java/com/example/JSPThree/view/XyzServlet.java
@@ -1,7 +1,6 @@
 package com.example.JSPThree.view;
 
 import com.example.JSPThree.controller.XyzController;
-//import com.example.JSPThree.model.Xyz;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -11,16 +10,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.List;
 
 @WebServlet("/xyz/*")
 public class XyzServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
-//    private Xyz xyzDAO;
-//
-//    public void init() {
-//        xyzDAO = new Xyz();
-//    }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
